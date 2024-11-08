95ed086201c9b74d3f7cee5800df01a2bea88f0b
ngxdjt <tyson.huynh@laetottenham.ac.uk>
2024-11-08 11:59:32 +0000

    Created basic cat class

---
+++ main.py
@@ -0,0 +1,15 @@
+class Cat:
+    # Constructor
+    # Creates a cat given its name and colour
+    def __init__(self,givenName,givenColour):
+        self.name = givenName
+        self.colour = givenColour
+
+# An instance of Cat
+# An instance is a specific occurrence of a class
+mimi = Cat("Mimi", "Brown")
+
+
+
+# \033[1A\033[K
+    
